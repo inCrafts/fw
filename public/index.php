6bc1903aa6ed12ed6f0d821733ca57b06694dd09
Alex <inCrafts@mail.ru>
2019-07-07 03:46:02 +0300

    Добавлена автозагрузка классов

--- public/index.php
+++ public/index.php
@@ -2,17 +2,30 @@
 
 $query = rtrim( $_SERVER['QUERY_STRING'], '/' );
 
+
+define( 'WWW', __DIR__ );
+define( 'CORE', dirname(__DIR__ ) . '/vendor/core');
+define( 'ROOT', dirname(__DIR__ ));
+define( 'APP', dirname(__DIR__ ) . '/app');
+
 require_once '../vendor/core/Router.php';
 require_once '../vendor/libs/functions.php';
 
-Router::add('posts/add', ['controller' => 'Posts', 'action' => 'add']);
-Router::add('posts', ['controller' => 'Posts', 'action' => 'index']);
-Router::add('', ['controller' => 'Main', 'action' => 'index']);
+//  User routes
+Router::add('^pages/?(?P<action>[a-z-]+)?$', ['controller' => 'Posts', 'action' => 'index']);
+
+// Default routes
+Router::add('^$', ['controller' => 'Main', 'action' => 'index']);
+Router::add('^(?P<controller>[a-z-]+)/?(?P<action>[a-z-]+)?$');
+
+spl_autoload_register( function( $class ) {
+    $file = APP . "/controllers/$class.php";
+    if ( is_file( $file ) ) {
+        require_once $file;
+    }
+});
+
 
 debug(Router::getRoutes());
 
-if ( Router::matchRoute( $query ) ) {
-    debug( Router::getRoute() );
-} else {
-    echo '404';
-}
+Router::dispatch( $query );
--- vendor/core/Router.php
+++ vendor/core/Router.php
@@ -8,30 +8,109 @@ class Router {
 //        echo 'Привет!';
 //
 //    }
+    /**
+     *   Таблица маршрутов
+     * @var array
+     */
+    protected static $routes = [];
+    /**
+     *   Текущий маршрут
+     * @var array
+     */
+    protected static $route = [];
+    //  в случае совпадения по регурярному выражэеннию выражению в виде ключа
 
-    protected static $routes = [];      // таблица маршрутов
-    protected static $route = [];       // текущий маршрут
-    // добавляет маршрут в таблицу в случае совпадения по регурярному выражэеннию выражению в виде ключа
+    /**
+     *   Добавляет текущий маршрут в таблицу маршрутов
+     * @param string $regexp Регулярное выражение маршрута
+     * @param array $route Маршрут ([controller, action, params])
+     */
     public static function add( $regexp, $route = [] ) {
         self::$routes[$regexp] = $route;
     }
-    // распечатка таблицы маршрутов
+
+    /**
+     * Возвращает таблицу маршрутов
+     * @return array
+     */
     public static function getRoutes() {
         return self::$routes;
     }
-    // распечатка текущего маршрута
+
+    /**
+     * Возвращает текущий маршрут ([controller, action, params])
+     * @return array
+     */
     public static function getRoute() {
         return self::$route;
     }
-    //  ищет совпадение с запросом в таблице маршрутов
+
+    /**
+     *   Botn URL в таблице маршрутов
+     * @param string $url Входящий маршрут
+     * @return bool
+     */
     public static function matchRoute( $url ) {
 
         foreach( self::$routes as $pattern => $route ) {
-            if ( $url === $pattern ) {
+            if ( preg_match( "#$pattern#i", $url, $matches )) {
+//                debug( $matches );
+                foreach ( $matches as $key => $val ) {
+                    if ( is_string( $key )) {
+                        $route[$key] = $val;
+                    }
+                }
+                if ( !isset( $route['action'] )) {
+                    $route['action'] = 'index';
+                }
                 self::$route = $route;
+                debug( $route );
                 return true;
             }
         }
         return false;
+    }
+
+    /**
+     *  Перенаправляет URL по корректному маршруту
+     * @param string $url Входящий URL
+     * return void
+     */
+    public static function dispatch( $url ) {
+
+        if ( self::matchRoute( $url )) {
+            $controller = self::upperCamelCase( self::$route['controller'] );
+            if ( class_exists( $controller )) {
+                $cObj = new $controller;
+
+                $action = self::lowerCamelCase( self::$route['action'] ) . 'Action';
+                debug( $action );
+                if ( method_exists( $cObj, $action ) ) {
+                    $cObj->$action();
+                } else {
+                    echo "Метод <b>$controller::$action</b> не найден.";
+                }
+            } else {
+                echo "Контроллер <b>$controller</b> не найден.";
+            }
+        } else {
+            http_response_code( 404 );
+            include '404.html';
+        }
+    }
+
+    /**
+     *  Приведит строку к верблюжьей нотации
+     * @param $name Строка
+     * @return измененная строка
+     */
+    protected static function upperCamelCase( $name ) {
+        return str_replace(' ', '', ucwords( str_replace('-', ' ', $name )));
+        debug($name);
+    }
+
+    protected static function lowerCamelCase( $name ) {
+        return lcfirst( self::upperCamelCase( $name ));
+
     }
 }
